e0f0ea644e1ee852ac482a21af9a3c85cd1e86ad
Tamara Hall <31778499+Highlyne@users.noreply.github.com>
2018-01-18 22:58:11 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var path = require("path");
 // Sets up the Express App to create server
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
